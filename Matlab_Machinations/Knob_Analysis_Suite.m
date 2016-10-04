e54c99c3009667f6d49c5c1ae5d43128450c578c
Sam Butensky <samuel.butensky@gmail.com>
2016-10-04 10:41:27 -0400

    Added loading config file structure

--- Matlab_Machinations/Knob_Analysis_Suite.m
+++ Matlab_Machinations/Knob_Analysis_Suite.m
@@ -50,18 +50,45 @@ datapath = 'C:\KnobAnalysis\Lab_Specific_Config_Files\';                        
 if ~exist(datapath,'dir')                                           %If the primary local data path doesn't already exist...
     mkdir(datapath);                                                %Make the primary local data path.
 end
+masterdatapath = 'C:\KnobAnalysis\Master_Lab_Specific_Config\';
+if ~exist(masterdatapath,'dir');
+    mkdir(masterdatapath);
+end
 % cd(datapath);
 Info = dir(datapath);
 if length(Info) < 3
-   Lab_Name = inputdlg('What is the name of the lab?', 'Lab Name', [1 50]);
-   Lab_NameNS = char(Lab_Name); Lab_NameNS = Lab_NameNS(Lab_NameNS~=' ');
-   functionspath = uigetdir('C:\', 'Where are your lab''s functions located?');
-   ConfigFileName = [Lab_NameNS 'Config.mat'];
-   config.Lab_Name = Lab_Name; config.path = functionspath; 
-   functionspath = [functionspath '\']; cd(functionspath); FctInfo = dir(functionspath);
-   FctInfo = {FctInfo.name}; FctInfo = FctInfo(3:end);
-   config.function_names = FctInfo;
-   cd(datapath); save(ConfigFileName, 'config');   
+    Load_or_Create = questdlg('Would you like to load a config file or add a new lab?',...
+        'Load Config or Add Lab',...
+        'Load Config', 'Add Lab', 'Cancel');
+    switch Load_or_Create
+        case 'Add Lab'
+            
+            Lab_Name = inputdlg('What is the name of the lab?', 'Lab Name', [1 50]);
+            Lab_NameNS = char(Lab_Name); Lab_NameNS = Lab_NameNS(Lab_NameNS~=' ');
+            functionspath = uigetdir('C:\', 'Where are your lab''s functions located?');
+            ConfigFileName = [Lab_NameNS 'Config.mat'];
+            config.Lab_Name = Lab_Name; config.path = functionspath;
+            functionspath = [functionspath '\']; cd(functionspath); FctInfo = dir(functionspath);
+            FctInfo = {FctInfo.name}; FctInfo = FctInfo(3:end);
+            config.function_names = FctInfo;
+            cd(datapath);
+            configfolderpath = uigetdir('C:\','Where are KnobAnalysis config files located?');
+            %config.masterconfigpath = configfolderpath;
+            save(ConfigFileName, 'config');
+            cd(configfolderpath); Info = dir(configfolderpath); ConfigNames = {Info.name};
+            BoolCompare = strcmpi(ConfigFileName,ConfigNames);
+            datapath = [datapath ConfigFileName];
+            if any(BoolCompare) == 0;
+                [Success,Message,MessageID] = copyfile(datapath,configfolderpath);
+            end
+            MasterConfigFileName = 'MasterConfig.mat'; cd(masterdatapath); save(MasterConfigFileName,'configfolderpath');
+        case 'Load Config'
+            [FileName,PathName] = uigetfile('*.mat','Select the configuration file');
+            datapath = 'C:\KnobAnalysis\Lab_Specific_Config_Files\';
+            olddatapath = [PathName FileName];
+            copyfile(olddatapath,datapath); %cd(datapath);
+%             load(FileName);
+    end
 end
 cd(datapath);
 Info = dir(datapath); FileNames = {Info.name}; FileNames = FileNames(3:end);
@@ -97,15 +124,24 @@ ConfigFile = uicontrol('Parent',fig,'Style','pushbutton','string','Load Config F
     'HorizontalAlignment', 'left', 'units', 'normalized', 'Position', [.55 .05 .3 .15],...
     'Fontsize', 12, 'Fontweight', 'bold') ;
 set(MoreLabs,'callback',{@AddNewLab,Lab_Name});
-set(ConfigFile,'callback',{@LoadConfigFile});
+set(ConfigFile,'callback',{@LoadConfigFile,Lab_Name});
 end
 
-function LoadConfigFile(~,~)
+function LoadConfigFile(~,~,Lab_Name)
 % datapath = 'C:\';
 [FileName,PathName] = uigetfile('*.mat','Select the configuration file');
-load(FileName);
-Lab_Name = config.Lab_Name; Functions = config.function_names;
-set(Analysis_Type,'string',Functions);
+datapath = 'C:\KnobAnalysis\Lab_Specific_Config_Files\';
+olddatapath = [PathName FileName];
+copyfile(olddatapath,datapath); cd(datapath);
+load(FileName); 
+
+% masterdatapath = 'C:\KnobAnalysis\Master_Lab_Specific_Config\';
+% cd(masterdatapath); load('MasterConfig.mat'); 
+New_Lab_Name = config.Lab_Name; %NewFunctions = config.function_names;
+LabNames = get(Lab_Name,'string'); LabNames(length(LabNames)+1) = New_Lab_Name;
+set(Lab_Name,'string',LabNames);
+% set(Analysis_Type,'string',Functions);
+% set(Lab_Name,'string',LabName);
 end
 
 function AddNewLab(~,~,Lab_Name)
@@ -113,6 +149,9 @@ datapath = 'C:\KnobAnalysis\Lab_Specific_Config_Files\';                        
 if ~exist(datapath,'dir')                                           %If the primary local data path doesn't already exist...
     mkdir(datapath);                                                %Make the primary local data path.
 end
+masterdatapath = 'C:\KnobAnalysis\Master_Lab_Specific_Config\';
+cd(masterdatapath); load('MasterConfig.mat'); 
+
 New_Lab_Name = inputdlg('What is the name of the lab?', 'Lab Name', [1 50]);
 Lab_NameNS = char(New_Lab_Name); Lab_NameNS = Lab_NameNS(Lab_NameNS~=' ');
 functionspath = uigetdir('C:\', 'Where are your lab''s functions located?');
@@ -124,6 +163,12 @@ config.function_names = FctInfo;
 cd(datapath); save(ConfigFileName, 'config');
 LabNames = get(Lab_Name,'string'); LabNames(length(LabNames)+1) = New_Lab_Name;
 set(Lab_Name,'string',LabNames);
+cd(configfolderpath); Info = dir(configfolderpath); ConfigNames = {Info.name};
+BoolCompare = strcmpi(ConfigFileName,ConfigNames);
+datapath = [datapath ConfigFileName];
+if any(BoolCompare) == 0;
+    [Success,Message,MessageID] = copyfile(datapath,configfolderpath);
+end
 end
 
 function LabName(hObject,~,Analysis_Type)
